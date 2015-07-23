cec51dae8a709cd615785c929ffdd6c34d4b0b7f
jbellmann <joerg.bellmann@zalando.de>
2015-07-23 20:19:49 +0200

    push only explicit configured tags, latest not included

--- src/main/java/org/jolokia/docker/maven/PushMojo.java
+++ src/main/java/org/jolokia/docker/maven/PushMojo.java
@@ -1,37 +1,45 @@
 package org.jolokia.docker.maven;
 
 import org.apache.maven.plugin.MojoExecutionException;
+
 import org.jolokia.docker.maven.access.AuthConfig;
 import org.jolokia.docker.maven.access.DockerAccess;
 import org.jolokia.docker.maven.access.DockerAccessException;
-import org.jolokia.docker.maven.config.*;
+import org.jolokia.docker.maven.config.BuildImageConfiguration;
+import org.jolokia.docker.maven.config.ImageConfiguration;
 import org.jolokia.docker.maven.util.ImageName;
 
 /**
- * Goal for pushing a data-docker container
+ * Goal for pushing a data-docker container.
  *
- * @author roland
- *
- * @goal push
- * @phase deploy
+ * @author  roland
+ * @goal    push
+ * @phase   deploy
  */
 public class PushMojo extends AbstractDockerMojo {
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void executeInternal(DockerAccess docker) throws DockerAccessException, MojoExecutionException {
+    public void executeInternal(final DockerAccess docker) throws DockerAccessException, MojoExecutionException {
         for (ImageConfiguration imageConfig : getImages()) {
             BuildImageConfiguration buildConfig = imageConfig.getBuildConfiguration();
             String name = imageConfig.getName();
             if (buildConfig != null) {
                 String configuredRegistry = getConfiguredRegistry(imageConfig);
-                AuthConfig authConfig = prepareAuthConfig(name,configuredRegistry);
+                AuthConfig authConfig = prepareAuthConfig(name, configuredRegistry);
 
-                docker.pushImage(name, authConfig, configuredRegistry);
+                // this leads to push for image with tag and push for 'latest' tag set by docker
+                // but we want only what is configured
+// getLog().debug("push image with 'name' " + name);
+// docker.pushImage(name, authConfig, configuredRegistry);
 
                 for (String tag : imageConfig.getBuildConfiguration().getTags()) {
+
                     if (tag != null) {
-                        docker.pushImage(new ImageName(name,tag).getFullName(), authConfig, configuredRegistry);
+                        getLog().debug("push image with 'name' " + name + "and 'tag' " + tag);
+                        docker.pushImage(new ImageName(name, tag).getFullName(), authConfig, configuredRegistry);
                     }
                 }
             }
