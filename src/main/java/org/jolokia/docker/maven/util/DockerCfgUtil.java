1b159486e303a7169f53d5cedaa4e94ebb7e452e
jbellmann <joerg.bellmann@zalando.de>
2015-07-23 20:26:29 +0200

    add pierone-cli specific behavior

--- src/main/java/org/jolokia/docker/maven/util/DockerCfgUtil.java
+++ src/main/java/org/jolokia/docker/maven/util/DockerCfgUtil.java
@@ -6,7 +6,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import java.util.Map;
+
 import org.apache.maven.plugin.MojoExecutionException;
+
+import org.bouncycastle.util.encoders.Base64;
 
 import org.jolokia.docker.maven.access.AuthConfig;
 
@@ -14,8 +18,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.google.common.base.Predicate;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 /**
  * @author  jbellmann
@@ -23,6 +29,7 @@ import com.google.common.collect.Lists;
 class DockerCfgUtil {
 
     private static final String EMPTY_STRING = "";
+    private static Splitter AUTH_SPLITTER = Splitter.on(":").omitEmptyStrings().trimResults();
 
     private final File userDockerCfgFile;
 
@@ -48,11 +55,31 @@ class DockerCfgUtil {
 
                 JSONObject registryConfig = data.getJSONObject(registryName);
 
-                final String username = getOrDefault(registryConfig, "username");
-                final String password = getOrDefault(registryConfig, "password");
+                String username = getOrDefault(registryConfig, "username");
+                String password = getOrDefault(registryConfig, "password");
                 final String email = getOrDefault(registryConfig, "email");
+
+                // special for pierone-cli
                 final String auth = getOrDefault(registryConfig, "auth");
-                AuthConfig authConfig = new AuthConfig(username, password, email, auth);
+
+                // typical case for pierone-cli created entries
+                if (username == null && password == null && auth != null && email.startsWith("no-mail-required")) {
+                    String decodedAuth = new String(Base64.decode(auth));
+                    Iterable<String> splitted = AUTH_SPLITTER.split(decodedAuth);
+                    if (Iterables.size(splitted) == 2) {
+                        username = Iterables.get(splitted, 0);
+                        password = Iterables.get(splitted, 1);
+                    }
+                }
+
+                Map<String, String> authConfigMap = Maps.newHashMap();
+                authConfigMap.put("username", username);
+                authConfigMap.put("password", password);
+                authConfigMap.put("email", email);
+                authConfigMap.put("auth", auth);
+                authConfigMap.put("serveraddress", registry);
+
+                AuthConfig authConfig = new AuthConfig(authConfigMap);
                 return authConfig;
 
             } catch (IOException e) {
@@ -83,6 +110,6 @@ class DockerCfgUtil {
             return value;
         } catch (JSONException e) { }
 
-        return EMPTY_STRING;
+        return null;
     }
 }
